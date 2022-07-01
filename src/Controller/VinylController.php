c207c29c5626c6c928cba49e501c22904e9d8dfd
Mayur Patel <pmayur0680@gmail.com>
2022-07-01 18:05:14 -0500

    updated base layout

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,38 +9,31 @@ use function Symfony\Component\String\u;
 
 class VinylController extends AbstractController
 {
-        #[Route('/')]
-        public function homepage(): Response
-        {
-//            $tracks = [
-//                'Gangsta\'s Paradise - Coolio',
-//                'Waterfalls - TLC',
-//                'Creep - Radiohead',
-//                'Kiss from a Rose - Seal',
-//                'On Bended Knee - Boyz II Men',
-//                'Fantasy - Mariah Carey',
-//            ];
-            $tracks = [
-                ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
-                ['song' => 'Waterfalls', 'artist' => 'TLC'],
-                ['song' => 'Creep', 'artist' => 'Radiohead'],
-                ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
-                ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
-                ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
-            ];
-            return $this->render('vinyl/homepage.html.twig', [
-                'title' => 'PB & Jams',
-                'tracks' => $tracks,
-            ]);
-        }
-        #[Route('/browse/{slug}')]
-        public function browse(string $slug = null): Response
-        {
-            if($slug) {
-                $title = 'Genre: '.u(str_replace('-', ' ', $slug))->title(true);
-            } else {
-                $title = "All Genres";
-            }
-            return new Response($title);
-        }
+    #[Route('/', name: 'app_homepage')]
+    public function homepage(): Response
+    {
+        $tracks = [
+            ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+            ['song' => 'Waterfalls', 'artist' => 'TLC'],
+            ['song' => 'Creep', 'artist' => 'Radiohead'],
+            ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
+            ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
+            ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
+        ];
+
+        return $this->render('vinyl/homepage.html.twig', [
+            'title' => 'PB & Jams',
+            'tracks' => $tracks,
+        ]);
+    }
+
+    #[Route('/browse/{slug}', name: 'app_browse')]
+    public function browse(string $slug = null): Response
+    {
+        $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
+
+        return $this->render('vinyl/browse.html.twig', [
+            'genre' => $genre
+        ]);
+    }
 }
